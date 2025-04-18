20276448ef85fed0fbd49192fb6e56582214bb8e
Elvin251 <emmmdov485@gmail.com>
2025-04-18 18:47:51 +0400

    b

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -10,5 +10,47 @@ byte digit = 100 ;
         System.out.println(ourNumber);
         System.out.println(area);
         System.out.println(digit);
+
+        byte number = 120;
+        System.out.println("number = " + number);
+
+        int ageb = 56;
+        System.out.println("age = " + ageb);
+
+        long length = 789665L;
+        System.out.println("length = " + length);
+
+        long random = 55443789654L;
+        System.out.println("random = " + random);
+
+        short count = 120;
+        System.out.println("count = " + count);
+
+        byte digita = 20;
+        System.out.println("digit = " + digita);
+
+        int price = 45000;
+        System.out.println("price = " + price);
+
+        long width = 119872212L;
+        System.out.println("width = " + width);
+
+        long variable = 10101010L;
+        System.out.println("variable = " + variable);
+
+        short counter = 12000;
+        System.out.println("counter = " + counter);
+        int agei = 35;
+        System.out.println("age = " + agei);
+
+        short ourNumberb = 125;
+        System.out.println("ourNumber = " + ourNumberb);
+
+        long areab = 29678533L;
+        System.out.println("area = " + areab);
+
+        byte digitb = 100;
+        System.out.println("digit = " + digitb);
     }
+
 }
