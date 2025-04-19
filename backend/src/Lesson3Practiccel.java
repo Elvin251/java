c735c7f68ccb75ca71aaca3f749667ad4849db93
Elvin251 <emmmdov485@gmail.com>
2025-04-19 17:10:26 +0400

    d

--- backend/src/Lesson3Practiccel.java
+++ backend/src/Lesson3Practiccel.java
@@ -1,6 +1,20 @@
 public class Lesson3Practiccel {
     public static void main(String[] args) {
-
-
+        boolean isThisNumber = true;
+        float number = 100.887f;
+        long myLong = 19212883212L;
+        char character = 'E';
+        System.out.println("isThisNumber: " + isThisNumber);
+        System.out.println("number: " + number);
+        System.out.println("myLong: " + myLong);
+        System.out.println("character: " + character);
+        byte ourPracticalByte = 19;
+        short myShort = 18999;
+        double radius = 185.821;
+        int digit = 7;
+        System.out.println("ourPracticalByte: " + ourPracticalByte);
+        System.out.println("myShort: " + myShort);
+        System.out.println("radius: " + radius);
+        System.out.println("digit: " + digit);
     }
 }
