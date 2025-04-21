873bd7a36685d4d6fb2bc72622fc0ae0065ef17f
Elvin251 <emmmdov485@gmail.com>
2025-04-21 22:28:53 +0400

    d

--- backend/src/lesson3homework.java
+++ backend/src/lesson3homework.java
@@ -1,14 +1,32 @@
 public class lesson3homework {
 
     public static void main(String[] args) {
+//        tap1 (google drive praktiki ders)
+        boolean isThisNumber = true;
+        float number = 100.887f;
+        long myLong = 192912883212L;
+        char character = 'E';
+        byte ourPracticalByte = 19;
+        short myShort = 18999;
+        double radius = 185.821;
+        int digit = 7;
+
+        System.out.println("isThisNumber: " + isThisNumber);
+        System.out.println("number: " + number);
+        System.out.println("myLong: " + myLong);
+        System.out.println("character: " + character);
+        System.out.println("ourPracticalByte: " + ourPracticalByte);
+        System.out.println("myShort: " + myShort);
+        System.out.println("radius: " + radius);
+        System.out.println("digit: " + digit);
+//tap2 (slayd ev isi 1)
         int age = 7;
         age = 14;
-
         byte b = 7;
         short s = 7;
         long l = 7;
-        double d = 7.9D;
-        float f = 7.9F;
+        double d = 7.9;
+        float f = 7.9f;
         char c = 'A';
         boolean hasHome = true;
 
@@ -20,6 +38,20 @@ public class lesson3homework {
         System.out.println("f: " + f);
         System.out.println("c: " + c);
         System.out.println("hasHome: " + hasHome);
-        System.out.println(" 10 eded açar");
+
+        System.out.println("10 addım");
+//        tap3(slayd ev isi 2)
+        int age1 = 25;
+        boolean hasJob = false;
+        char studentGrade = 'C';
+        double carPrice = 25000;
+        String employeeName = "Malik";
+
+
+        System.out.println("age : " + age1);
+        System.out.println("hasJob : " + hasJob);
+        System.out.println("studentGrade : " + studentGrade);
+        System.out.println("carPrice : " + carPrice);
+        System.out.println("employeeName : " + employeeName);
 
 }}
