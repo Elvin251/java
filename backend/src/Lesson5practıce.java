c9f770ce0698b2f6d3e8a99524d55172fd9887d3
Elvin251 <emmmdov485@gmail.com>
2025-04-26 17:07:21 +0400

    d

---
+++ backend/src/Lesson5practıce.java
@@ -0,0 +1,27 @@
+public class Lesson5practıce {
+    public static void main(String[] args) {
+        // 1
+        String salary = "2500";
+        int salaryInt = Integer.parseInt(salary);
+        System.out.println(salaryInt);
+
+        // 2
+        String number = "800";
+        int numberInt = Integer.parseInt(number);
+        System.out.println(numberInt);
+
+        // 3
+        char har = '%';
+        int characterInt = har;
+        System.out.println(characterInt);
+
+        // 4
+        short maxShort = Short.MAX_VALUE;
+        int result = maxShort + 100;
+        System.out.println("Maximum short is: " + result);
+//5
+        String string1 = "200bc";
+        int string1Int = Integer.parseInt(string1);
+        System.out.println(string1Int);
+    }
+}
