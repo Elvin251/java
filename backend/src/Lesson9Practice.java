fc0d72e226d74de93ff0b270018b3957ba9d4028
Elvin251 <emmmdov485@gmail.com>
2025-05-10 17:24:19 +0400

    lesson9 practice

--- backend/src/Lesson9Practice.java
+++ backend/src/Lesson9Practice.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-public class Lesson9Practice {
 
+public class Lesson9Practice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        ders praktikasi 2
-//1
+
+        // Ders praktikasi 2
+        // 1
         int i = 2;
         while (i < 5) {
             System.out.println(i);
             i++;
         }
-//        7
+
+        // 7
         int i2 = 1;
         int sum = 0;
         while (i2 <= 100) {
@@ -18,7 +20,8 @@ public class Lesson9Practice {
             i2++;
         }
         System.out.println("Cəm: " + sum);
-//        8
+
+        // 8
         int i3 = 1;
         while (i3 <= 100) {
             if (i3 % 7 == 0) {
@@ -26,50 +29,55 @@ public class Lesson9Practice {
             }
             i3++;
         }
-//        9
+
+        // 9
         System.out.print("Ədəd daxil edin: ");
         int number = input.nextInt();
-
         if (number % 2 == 0) {
             System.out.println("Ədəd cütdür.");
         } else {
             System.out.println("Ədəd təkdir.");
         }
-//        10
+
+        // 10
         int i4 = 1;
         while (i4 <= 200) {
-            if (i % 3 == 0 && i % 7 == 0) {
-                System.out.println(i);
+            if (i4 % 3 == 0 && i4 % 7 == 0) {
+                System.out.println(i4);
             }
             i4++;
-//            ders praktikasi 3
-//            1
-            int i5 = 50;
-            while (i5 >= 20) {
-                System.out.println(i5);
-                i5--;
+        }
+
+        // Ders praktikasi 3
+        // 1
+        int i5 = 50;
+        while (i5 >= 20) {
+            System.out.println(i5);
+            i5--;
+        }
+
+        // 2
+        double i6 = 0.0;
+        while (i6 <= 5.0) {
+            System.out.printf("eded=%.1f\n", i6);
+            i6 += 0.1;
+        }
+
+        // Ev isi 2
+        // 1
+        int i7 = 16;
+        while (i7 < 40) {
+            System.out.println(i7);
+            i7++;
+        }
+
+        // 2
+        int i8 = 21;
+        while (i8 < 50) {
+            if (i8 % 2 != 0) {
+                System.out.println(i8);
             }
-//            2
-            double i6 = 0.0;
-            while (i6 <= 5.0) {
-                System.out.printf("eded=", i6);
-                i6 += 0.1;
-//                ev isi 2
-//                1
-                int i7 = 16;
-                while (i7 < 40) {
-                    System.out.println(i7);
-                    i7++;
-//                    2
-                    int i8 = 21;
-                    while (i8 < 50) {
-                        if (i8 % 2 != 0) {
-                            System.out.println(i8);
-                        }
-                        i8++;
-                    }
-                }
-            }
+            i8++;
         }
     }
 }
