38c64c1be27066b03eb356223dd4af08f6f5469e
Elvin251 <emmmdov485@gmail.com>
2025-05-24 17:23:27 +0400

    lesson13 practice

--- backend/src/Lesson13Practice/Main.java
+++ backend/src/Lesson13Practice/Main.java
@@ -3,14 +3,14 @@ package Lesson13Practice;
 public class Main {
     public static void main(String[] args) {
 
-        RestaurantFood food = new RestaurantFood("Toyuq kababı", 8.5, "toyuq, ədviyyat, tərəvəz");
+        RestaurantFood food = new RestaurantFood("Toyuq kababı", 8.5, "toyuq, ədviyyat, tərəvəz" );
 
 
         Order order = new Order(food, 2);
         order.showOrder();
 
 
-        Bank userCard = new Bank("1234-5678-9012-3456", 20.0);  // balans 20 AZN-dir
+        Bank userCard = new Bank("1234-5678-9012-3456", 20.0);  
 
         double totalAmount = order.calculateTotal();
         System.out.println("Ödəniləcək məbləğ: " + totalAmount + " AZN");
