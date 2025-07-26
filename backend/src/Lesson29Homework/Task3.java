185a686137cb525c632c40462343ff47ff84b5a4
Elvin251 <emmmdov485@gmail.com>
2025-07-26 17:15:52 +0400

    Lesson30Practice1

--- backend/src/Lesson29Homework/Task3.java
+++ backend/src/Lesson29Homework/Task3.java
@@ -4,6 +4,6 @@ public class Task3 {
         String str = "Hello";
         char[] dest = {'a', 'a', 'd', 'd', 'a'};
         str.getChars(2, 4, dest, 1); // "ll" hissəsini 1-ci indeksdən başlayaraq yerləşdir
-        System.out.println(dest); 
+        System.out.println(dest);
     }
 }
