38f4332105b90bde6840ea29e6b052f39bf79f0e
Elvin251 <emmmdov485@gmail.com>
2026-01-21 22:00:01 +0400

    Lesson70

--- spring/demo2/src/main/java/az/developia/demo2/Lesson70/SecurityConfig.java
+++ spring/demo2/src/main/java/az/developia/demo2/Lesson70/SecurityConfig.java
@@ -1,12 +1,30 @@
 package az.developia.demo2.Lesson70;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/users/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .userDetailsService(userDetailsService)
+                .httpBasic();
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
