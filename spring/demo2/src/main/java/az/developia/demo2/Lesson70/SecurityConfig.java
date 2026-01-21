642398ffb23c91a769eff08261442feff52f9136
Elvin251 <emmmdov485@gmail.com>
2026-01-21 22:02:16 +0400

    lesson70

--- spring/demo2/src/main/java/az/developia/demo2/Lesson70/SecurityConfig.java
+++ spring/demo2/src/main/java/az/developia/demo2/Lesson70/SecurityConfig.java
@@ -1,12 +1,8 @@
 package az.developia.demo2.Lesson70;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -14,10 +10,8 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/users/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .userDetailsService(userDetailsService)
                 .httpBasic();
 
         return http.build();
